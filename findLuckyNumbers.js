1d8d05dc523852ed04721862ac5fb39f811a90d0
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-15 10:39:18 -0700

    Challenge complete

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,15 @@
 // Write your code below this line.
+const luckyNumbers = (num) => {
+  const randomArr = [];
+  for (let i = 0; i < num; i++) {
+    const random = Math.floor(Math.random() * 10 + 1);
+    if (randomArr.length > 0) {
+      randomArr.includes(random) ? true : randomArr.push(random);
+    } else {
+      randomArr.push(random);
+    }
+  }
+  return randomArr;
+};
+
+console.log(luckyNumbers(10));
